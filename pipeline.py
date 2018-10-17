6fec84ff1aadffb3d5f3098f1f322ecc5f24271a
Syed Hussain Ather <shussainather@gmail.com>
2018-10-17 14:10:02 -0400

    initial commit

---
+++ pipeline.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+f = "/data/athersh/Undetermined_S0_L001_R1_001.fastq.gz"
+
+with open(f) as file:
+
+
+def retrieve_data(input):
+	for i in line.readlines()
+	
+
+def testing(x):
+	for a in range(100):
+
+# I hope that I can put forward something useful from this function
+# in using it for star-align or bowtie2 to align.
