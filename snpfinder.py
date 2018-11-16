10906533091541f59c11a33f87155b084715728c
Syed Hussain Ather <shussainather@gmail.com>
2018-11-16 14:37:26 -0500

    initial commit

---
+++ snpfinder.py
@@ -0,0 +1,10 @@
+# Used to locate and analyze the SNPs in RNA-Seq data
+
+sample = ["Undetermined_S0_L001_R1_001.fasta", 
+		  "Undetermined_S0_L002_R1_001.fasta", 
+		  "Undetermined_S0_L001_R2_001.fasta",
+		  "Undetermined_S0_L002_R2_001.fasta"]
+
+o = write(
+
+for i in f.readlines():
