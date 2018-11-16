a723181c034284467e31718a5afb83204e017e73
Syed Hussain Ather <shussainather@gmail.com>
2018-11-16 15:39:40 -0500

    initial commit

--- snpfinder.py
+++ snpfinder.py
@@ -1,10 +1,19 @@
 # Used to locate and analyze the SNPs in RNA-Seq data
 
-sample = ["Undetermined_S0_L001_R1_001.fasta", 
-		  "Undetermined_S0_L002_R1_001.fasta", 
-		  "Undetermined_S0_L001_R2_001.fasta",
-		  "Undetermined_S0_L002_R2_001.fasta"]
+samples = {"Undetermined_S0_L001_R1_001.fasta":"snp/L001_R1_report", 
+   		  "Undetermined_S0_L002_R1_001.fasta":"snp/L002_R1_report", 
+		  "Undetermined_S0_L001_R2_001.fasta":"snp/L001_R2_report",
+		  "Undetermined_S0_L002_R2_001.fasta":"snp/L002L_R2_report"}
 
-o = write(
+ref = "Danio_rerio.GRCz10.cdna.all.fa"
 
-for i in f.readlines():
+for sample in samples
+	f = open(str(sample), "r")
+	o = open(str(samples[sample]), "a")
+	count = 0
+	for line in f.readlines():
+		if line[0] != ">":
+			sampleline = line
+		for line in 
+			count += 1
+
