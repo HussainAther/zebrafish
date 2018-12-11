6356a805f7c2a8825f72489d9c233afbcf3cb49c
Syed Hussain Ather <shussainather@gmail.com>
2018-12-11 09:22:06 -0500

    fixed directory syntax

--- gatk.bat
+++ gatk.bat
@@ -4,11 +4,11 @@ set -e
 
 module load GATK/3.5-0 || exit 1
 
-cd /data/$USER/test_data/GATK
+cd /data/athersh/
 
 # use 8 GB memory
 java -Xmx8g -Djava.io.tmpdir=/lscratch/$SLURM_JOBID -jar $GATK_JAR \
 	-T CountReads \
 	-R Danio_rerio.GRCz10.cdna.all.fa \
-	-I 2016/R1/Aligned.out.bam.sorted \
+	-I 2016/R1/Aligned.out.sorted.bam \
 	-et NO_ET -K $GATK_KEY
