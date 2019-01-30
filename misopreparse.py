f0e886fb0343b9e62ff3c90d61c96f3418a09e4e
Syed Hussain Ather <shussainather@gmail.com>
2019-01-30 18:33:40 -0500

    all chr

--- misopreparse.py
+++ misopreparse.py
@@ -1,8 +1,8 @@
-chr_range = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"]
+chr_range = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "M"]
 
-o = open("/data/athersh/Danio_rerio.GRCz11.94.fixed.gff3", "w") 
+o = open("/data/athersh/Danio_rerio.GRCz11.95.fixed.gff3", "w") 
 
-with open("/data/athersh/Danio_rerio.GRCz11.94.gff3", "r") as file:
+with open("/data/athersh/Danio_rerio.GRCz11.95.gff3", "r") as file:
     for line in file:
         if line.split("\t")[0] in chr_range:
             o.write("chr" + str(line))
