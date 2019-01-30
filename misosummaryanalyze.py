f443ce7b4c25c3b568db3c8f6ac38319498ae018
Syed Hussain Ather <shussainather@gmail.com>
2019-01-30 18:34:10 -0500

    all files

--- misosummaryanalyze.py
+++ misosummaryanalyze.py
@@ -6,20 +6,17 @@ f = open("miso_output_2019_1_summary/summary/miso_output_2019_1.miso_summary.par
 
 f.write(df.sort_values(by=["miso_posterior_mean"]).head()
 
-
 df = pd.read_csv("miso_output_2019_7_summary/summary/miso_output_2019_7.miso_summary", sep="\t")
 
 f = open("miso_output_2019_7_summary/summary/miso_output_2019_7.miso_summary.parsed", "w")
 
 f.write(df.sort_values(by=["miso_posterior_mean"]).head()
 
-
 df = pd.read_csv("miso_output_2019_1control_summary/summary/miso_output_2019_1control.miso_summary", sep="\t")
 
 f = open("miso_output_2019_1control_summary/summary/miso_output_2019_1control.miso_summary.parsed", "w")
 
 f.write(df.sort_values(by=["miso_posterior_mean"]).head()
-
 
 df = pd.read_csv("miso_output_2019_7contrl_summary/summary/miso_output_2019_7control.miso_summary", sep="\t")
 
