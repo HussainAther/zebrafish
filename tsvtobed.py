a3d12f12ba2b472a3407d614b20f73f61e860cc3
Syed Hussain Ather <shussainather@gmail.com>
2019-01-31 18:56:04 -0500

    convert to df fixed

--- tsvtobed.py
+++ tsvtobed.py
@@ -6,13 +6,12 @@ chr = list(df["chrom"])
 start = list(df["mRNA_starts"])
 end = list(df["mRNA_ends"])
 name = list(df["event_name"])
-score = list(len(list(df["chrom"]))*"\t")
+score = list(len(list(df["chrom"]))*"0")
 strand = list(df["strand"])
 
-all = [chr, start, end, name, score, strand]
-labels = ["chr", "start", "end", "name", "score", "strand"]
+all = [("chr", chr), ("start", start), ("end", end), ("name", name), ("score", score), ("strand", strand)]
 
-beddf = pd.DataFrame.from_records(all, columns=labels)
+beddf = pd.DataFrame.from_items(all, index).set_index("event_name")
 
 with open("miso_output_1.bed", "w") as file:
     file.write(pd.DataFrame.to_csv(beddf, sep="\t"))
