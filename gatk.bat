9786524ee3ebb480bfac20d18b10855e3f80e73f
Syed Hussain Ather <shussainather@gmail.com>
2019-01-31 19:02:29 -0500

    updated for samples

--- gatk.bat
+++ gatk.bat
@@ -2,13 +2,17 @@
 # gatk.bat
 set -e
 
-module load GATK/3.5-0 || exit 1
+module load GATK || exit 1
 
 cd /data/athersh/
 
 # use 8 GB memory
-java -Xmx8g -Djava.io.tmpdir=/lscratch/$SLURM_JOBID -jar $GATK_JAR \
+#java -Xmx8g -Djava.io.tmpdir=/lscratch/$SLURM_JOBID -jar $GATK_JAR \
+#	-T CountReads \
+#	-R Danio_rerio.GRCz10.cdna.all.fa \
+#	-I 2016/R1/Aligned.out.sorted.picard.bam \
+
+java -Xmx80g -Djava.io.tmpdir=/lscratch/$SLURM_JOBID -jar $GATK_JAR \
 	-T CountReads \
 	-R Danio_rerio.GRCz10.cdna.all.fa \
-	-I 2016/R1/Aligned.out.sorted.bam \
-	-et NO_ET -K $GATK_KEY
+	-I 2016/R1/Aligned.out.sorted.picard.bam
