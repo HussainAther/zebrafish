d0ab557a3d553f2037040d93c050775585d578db
Syed Hussain Ather <shussainather@gmail.com>
2019-01-31 19:09:15 -0500

    syntax input

--- tsvtobed.py
+++ tsvtobed.py
@@ -18,10 +18,10 @@ def create_bed(input, output):
         file.write(pd.DataFrame.to_csv(beddf, sep="\t"))
     return
 
-input_output = {"summary_miso_output_2019_1/summary_miso_output_2019_1.miso_summary":"miso_output_2019_1.bed",
-        "summary_miso_output_2019_7/summary_miso_output_2019_7.miso_summary":"miso_output_2019_7.bed",
-        "summary_miso_output_2019_1control/summary_miso_output_2019_1control.miso_summary":"miso_output_2019_1control.bed",
-        "summary_miso_output_2019_7control/summary_miso_output_2019_7control.miso_summary":"miso_output_2019_7control.bed"}
+input_output = {"summary_miso_output_2019_1/summary/miso_output_2019_1.miso_summary":"miso_output_2019_1.bed",
+        "summary_miso_output_2019_7/summary/miso_output_2019_7.miso_summary":"miso_output_2019_7.bed",
+        "summary_miso_output_2019_1control/summary/miso_output_2019_1control.miso_summary":"miso_output_2019_1control.bed",
+        "summary_miso_output_2019_7control/summary/miso_output_2019_7control.miso_summary":"miso_output_2019_7control.bed"}
 
 for i in input_output:
     create_bed(i, input_output[i])
